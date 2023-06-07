0cd919771eadd4d4fc2d60e94873f1040f46d39d
Asaurus1 <alexandermartin2018@u.northwestern.edu>
2023-06-06 23:48:01 -0700

    Update setup.py to include py.typed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf8') as fh:
 
 setuptools.setup(
     name="streamlit-qs",
-    version="0.1.0",
+    version="0.1.1",
     author="Alexander Martin",
     author_email="fauxjunk-1@yahoo.com",
     description="A small library to add extra functionality on top of streamlit's experimental_[get/set]_query_string API",
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Asaurus1/streamlit-qs",
     packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
+    package_data={"streamlit_qs": ["py.typed"]},
     install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
