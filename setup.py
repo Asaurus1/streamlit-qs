4b1f1f685df9e503e7f29e5ef54e6bb01f8cfb0a
Asaurus1 <alexandermartin2018@u.northwestern.edu>
2024-01-27 22:21:09 -0800

    Bump version number to 0.3.0 in setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open("README.md", "r", encoding='utf8') as fh:
 
 setuptools.setup(
     name="streamlit-qs",
-    version="0.2.0",
+    version="0.3.0",
     author="Alexander Martin",
     author_email="fauxjunk-1@yahoo.com",
-    description="A small library to add extra functionality on top of streamlit's experimental_[get/set]_query_string API",
+    description="A small library to add extra functionality on top of streamlit's st.query_params API",
     license="Apache 2",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Asaurus1/streamlit-qs",
     packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
     package_data={"streamlit_qs": ["py.typed"]},
-    install_requires=["streamlit>=1.27.0"],
+    install_requires=["streamlit>=1.30.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
